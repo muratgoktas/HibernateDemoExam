1ade456c00480816f7bc197540721c3730963c3c
Murat Goktas <mcs.goktas@gmail.com>
2022-05-26 17:13:23 +0300

    Mg-04) Created City class.

---
+++ src/com/hibernateDemo/City.java
@@ -0,0 +1,41 @@
+package com.hibernateDemo;
+
+
+public class City {
+	private int id;
+	private String name;
+	private String countryCode;
+	private String district;
+	private int population;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountryCode() {
+		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+	
+}
